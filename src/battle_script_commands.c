db5cf1836ecb34696f962788bf682008eb2c4785
Soul-8691 <peterswanton191@bellsouth.net>
2025-06-24 09:19:10 -0400

    Merge branch 'saveblock' of https://github.com/ghoulslash/pokeemerald into ygo

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -3299,7 +3299,11 @@ static void Cmd_getexp(void)
                 item = GetMonData(&gPlayerParty[i], MON_DATA_HELD_ITEM);
 
                 if (item == ITEM_ENIGMA_BERRY)
+                    #ifndef FREE_ENIGMA_BERRY
                     holdEffect = gSaveBlock1Ptr->enigmaBerry.holdEffect;
+                    #else
+                    holdEffect = 0;
+                    #endif
                 else
                     holdEffect = GetItemHoldEffect(item);
 
@@ -3338,7 +3342,11 @@ static void Cmd_getexp(void)
             item = GetMonData(&gPlayerParty[gBattleStruct->expGetterMonId], MON_DATA_HELD_ITEM);
 
             if (item == ITEM_ENIGMA_BERRY)
+                #ifndef FREE_ENIGMA_BERRY
                 holdEffect = gSaveBlock1Ptr->enigmaBerry.holdEffect;
+                #else
+                holdEffect = 0;
+                #endif
             else
                 holdEffect = GetItemHoldEffect(item);
 
