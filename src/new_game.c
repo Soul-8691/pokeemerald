8a8d291070d17b32a3539e00fc3979f7e1b896bb
Soul-8691 <peterswanton191@bellsouth.net>
2025-10-27 10:18:23 -0400

    Merge branch 'unbound-quest-menu' of https://github.com/Soul-8691/pokeemerald into radiant-orange

--- src/new_game.c
+++ src/new_game.c
@@ -44,6 +44,7 @@
 #include "berry_powder.h"
 #include "mystery_gift.h"
 #include "union_room_chat.h"
+#include "quests.h"
 #include "constants/items.h"
 
 extern const u8 EventScript_ResetAllMapFlags[];
@@ -203,6 +204,7 @@ void NewGameInitData(void)
     WipeTrainerNameRecords();
     ResetTrainerHillResults();
     ResetContestLinkResults();
+    QuestMenu_ResetMenuSaveData();
 }
 
 static void ResetMiniGamesRecords(void)
